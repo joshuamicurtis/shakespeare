57b9d7fbdb063cd2f432718d6413144b50267561
Joshua Curtis <joshua.mi.curtis@gmail.com>
2016-10-24 19:14:04 -0400

    Can know add characters, and their play foriegn key to DB

--- createDB.py
+++ createDB.py
@@ -13,20 +13,37 @@ cur.execute('''DROP TABLE IF EXISTS Lines ''')
 
 #Create Tables
 cur.execute('''CREATE TABLE IF NOT EXISTS Plays 
-    (id INTEGER PRIMARY KEY, name TEXT, characters INTEGER)''') 
+    (id INTEGER PRIMARY KEY, name TEXT)''') 
 cur.execute('''CREATE TABLE IF NOT EXISTS Characters
-    (id INTEGER PRIMARY KEY, name TEXT, speeches)''')
+    (id INTEGER PRIMARY KEY, name TEXT, playID INTEGER)''')
 cur.execute('''CREATE TABLE IF NOT EXISTS Speeches
     (id INTEGER PRIMARY KEY, act INTEGER, number INTEGER)''')
 cur.execute('''CREATE TABLE IF NOT EXISTS Lines
     (id INTEGER PRIMARY KEY, number INTEGER, words TEXT)''') 
     
-#Insert play name into 
+ 
 for fileName in os.listdir('.'):
     if fileName.lower().endswith('.txt'):
         fileName = fileName[:-4]
+        #Insert play name into DB
         cur.execute('''INSERT OR IGNORE INTO PLAYS (name)
             VALUES(?)''',(fileName,))
-        print fileName, 
+        playKey = cur.lastrowid
+        #Insert character names into DB
+        fileName += ".txt"
+        print fileName
+        file=open(fileName,"r+")
+        for word in file.read().split():
+            word = word.lstrip('\'\"-,.:;!?]')
+            word = word.rstrip('\'\"-,.:;!?]')  
+            if word.isupper() and len(word) > 3 and word != "SCENE":  
+                cur.execute('''SELECT COUNT(name) from CHARACTERS 
+                                WHERE name = (?)''',(word,))
+                if list(cur.fetchone())[0] < 1:
+                    cur.execute('''INSERT OR IGNORE INTO CHARACTERS (name, 
+                    playID) VALUES(?,?)''',(word, playKey,))
+        print fileName, "added to database"
+        
+       
 conn.commit()
 cur.close()
