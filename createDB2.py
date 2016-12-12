6541441e40374ac33ed571d5d7368c1c56caed29
Joshua Curtis <joshua.mi.curtis@gmail.com>
2016-12-12 13:41:03 -0500

    added speechID to line table. Cleaned up DB redundancies

--- createDB2.py
+++ createDB2.py
@@ -2,7 +2,7 @@ import sys
 import os
 import sqlite3
 
-conn = sqlite3.connect('shakespearePlays.sqlite')
+conn = sqlite3.connect('shakespearePlays2.sqlite')
 cur = conn.cursor()
 conn.text_factory = str
 #Drop tables if they already exist
@@ -20,7 +20,8 @@ cur.execute('''CREATE TABLE IF NOT EXISTS Speeches
     (id INTEGER PRIMARY KEY, speech TEXT, characterID INTEGER,
     act TEXT, number INTEGER)''')
 cur.execute('''CREATE TABLE IF NOT EXISTS Lines
-    (id INTEGER PRIMARY KEY, line TEXT, speechID, number INTEGER)''') 
+    (id INTEGER PRIMARY KEY, line TEXT, characterID INTEGER, 
+    speechID, act TEXT, number INTEGER)''') 
 cur.execute('''CREATE TABLE IF NOT EXISTS Words
     (id INTEGER PRIMARY KEY, characterID INTEGER, playID INTEGER, speechID,
     lineID INTEGER, act TEXT, number INTEGER)''') 
@@ -33,46 +34,55 @@ for fileName in os.listdir('.'):
         cur.execute('''INSERT OR IGNORE INTO PLAYS (name)
             VALUES(?)''',(playName,))
         playKey = cur.lastrowid
-        
-        #Insert data into character table of DB
-        file=open(fileName,"r+")
-        for word in file.read().split():
-            word = word.lstrip('\'\"-,.:;!?]')
-            word = word.rstrip('\'\"-,.:;!?]')  
-            if word.isupper() and len(word) > 3 and word != "SCENE" and word != "PRINCE":  
-                # Check if character is already in database
-                cur.execute('''SELECT COUNT(name) from CHARACTERS 
-                                WHERE name = (?)''',(word,))
-                if list(cur.fetchone())[0] < 1:
-                    cur.execute('''INSERT OR IGNORE INTO CHARACTERS (name, 
-                    playID) VALUES(?,?)''',(word, playKey,))
-        
-        # Insert data into speech table of DB
-        file.seek(0)
-        speech = ""
+           
         character = ""
+        speech = ""
         speechNumber = 0
         lineNumber = 0
         newCharacter = False
-        for line in file:      
-            # Check if a new act is beginning
+        file=open(fileName,"r+")
+        for line in file:   
+            if line.isupper():
+                for word in line.split():
+                    word = word.lstrip('\'\"-,.:;!?]')
+                    word = word.rstrip('\'\"-,.:;!?]')  
+                    if len(word) > 3 and word != "SCENE" and word != "PRINCE": 
+                        character = word
+                        # Check if character is already in database
+                        cur.execute('''SELECT COUNT(name) from CHARACTERS 
+                                        WHERE name = (?)''',(character,))
+                        if list(cur.fetchone())[0] < 1:
+                            #Insert new character into character table of DB
+                            cur.execute('''INSERT OR IGNORE INTO CHARACTERS (name, 
+                            playID) VALUES(?,?)''',(character, playKey,))
             if line.isupper() and line[0:3] == "ACT":
                 act = line[3:].rstrip("\n")
+            
             # Check if a new character is speeking
-            if line.isupper() and line[0:3] != "ACT":
+            elif line.isupper() and line[0:3] != "ACT":
+                # Get the character ID of the newCharacter
+                cur.execute(''' SELECT Characters.id 
+                                from CHARACTERS WHERE name = 
+                                (?)''', (character,))
+                characterID = cur.fetchone()
                 if character != "" and characterID != None:
                     # Insert the previous speech into the DB
                     cur.execute('''INSERT OR IGNORE INTO SPEECHES (speech, 
                                 characterID, act, number) 
                                 VALUES(?,?,?,?)''', (speech, characterID[0], 
                                 act, speechNumber, ))
+                    lines = speech.split("\n")
+                    #print lines
+                    #for line in lines:
+                        
                 speechNumber += 1
                 character = line.rstrip("\n")
                 newCharacter = True
                 speech = ""
-            if line.isupper() == False and line[0:4] != "Exit" and line[0:5] != "Enter" and line != "\n":
-                speech += " " + line       
+            if line.isupper() == False:
+                speech += " " + line              
                 if newCharacter == True:
+                    # Get the character ID of the newCharacter
                     cur.execute(''' SELECT Characters.id 
                                     from CHARACTERS WHERE name = 
                                     (?)''', (character,))
@@ -86,11 +96,9 @@ for fileName in os.listdir('.'):
                         lineNumber += 1
                         # Insert data into line table of DB
                         cur.execute('''INSERT OR IGNORE INTO LINES (line, 
-                                    speechID, number) VALUES(?,?,?)''', 
-                                    (line, speechNumber, lineNumber, ))  
-            
-            
-                
+                                    characterID, act, number) 
+                                    VALUES(?,?,?,?)''', (line, characterID[0], 
+                                    act, lineNumber, )) 
         conn.commit()
         print fileName, "added to database"     
 
